70723c5c0ecef5f21e53a485a464db2bfe59a61e
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2026-01-23 18:59:51 +0300

    pom файл страницы веб форм

--- src/main/java/pages/WebFormPage.java
+++ src/main/java/pages/WebFormPage.java
@@ -14,19 +14,20 @@ public class WebFormPage {
     private String dropdownValueTwo = "2";
     private String dropdownValueThree = "3";
 
-    private String dropdownDatalistSF = "value=\"San Francisco\"";
-    private String dropdownDatalistNY = "value=\"New York\"";
-    private String dropdownDatalistS = "value=\"Seattle\"";
-    private String dropdownDatalistLA = "value=\"Los Angeles\"";
-    private String dropdownDatalistC = "value=\"Chicago\"";
+    private String dropDownDataList = "name=\"my-datalist\"";
+    private String dropdownDatalistValueSF = "San Francisco";
+    private String dropdownDatalistValueNY = "New York";
+    private String dropdownDatalistValueS = "Seattle";
+    private String dropdownDatalistValueLA = "Los Angeles";
+    private String dropdownDatalistValueC = "Chicago";
 
     private String fileInput = "name=\"my-file\"";
 
     private String checkedCheckbox = "id=\"my-check-1\"";
-    private String defoultCheckbox = "id=\"my-check-2\"";
+    private String defaultCheckbox = "id=\"my-check-2\"";
 
     private String checkedRadio = "id=\"my-radio-1\"";
-    private String defoultRadio = "id=\"my-radio-2\"";
+    private String defaultRadio = "id=\"my-radio-2\"";
 
     public Locator loc_TextUnput() {
         return generic.getById(textInput);
@@ -50,6 +51,58 @@ public class WebFormPage {
 
     public void selectDropdownOptionThree() {
         generic.selectByValue(loc_Dropdown(), dropdownValueThree);
+    }
+
+    public Locator loc_DataListInput() {
+        return generic.getByLocator(dropDownDataList);
+    }
+
+    public Locator loc_FileInput() {
+        return generic.getByLocator(fileInput);
+    }
+
+    public Locator loc_CheckedCheckbox() {
+        return generic.getByLocator(checkedCheckbox);
+    }
+
+    public Locator loc_DefaultCheckbox() {
+        return generic.getByLocator(defaultCheckbox);
+    }
+
+    public Locator loc_CheckedRadio() {
+        return generic.getByLocator(checkedRadio);
+    }
+
+    public Locator loc_DefaultRadio() {
+        return generic.getByLocator(defaultRadio);
+    }
+
+    public void fillDataList(String value) {
+        generic.fill(loc_DataListInput(), value);
+    }
+
+    public void selectDataListSF() {
+        fillDataList(dropdownDatalistValueSF);
+    }
+
+    public void selectDataListNY() {
+        fillDataList(dropdownDatalistValueNY);
+    }
+
+    public void selectDataListSeattle() {
+        fillDataList(dropdownDatalistValueS);
+    }
+
+    public void selectDataListLA() {
+        fillDataList(dropdownDatalistValueLA);
+    }
+
+    public void selectDataListChicago() {
+        fillDataList(dropdownDatalistValueC);
+    }
+
+    public String getDataListValue() {
+        return loc_DataListInput().inputValue();
     }
 
 }
